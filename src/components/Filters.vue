1f163b628d2d7a1c0c1fb31397cc43d133c2010b
GauravVQ <gauravvanjani751@gmail.com>
2022-01-14 20:20:44 +0530

    ui updated

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,7 +1,59 @@
 <template>
 <div>
  <div class="heading">Select a date: <date-picker v-model="selectedDate" valueType="format" default-value :confirm="getDate()"></date-picker></div>
- <div class="loader" v-if="loading">Hello</div>
+ <div class="loader" v-if="loading">
+   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background:transparent; display: block; shape-rendering: auto;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+     <g transform="rotate(0 50 50)">
+       <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+         <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.9166666666666666s" repeatCount="indefinite"/>
+       </rect>
+     </g><g transform="rotate(30 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.8333333333333334s" repeatCount="indefinite"/>
+     </rect>
+   </g><g transform="rotate(60 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.75s" repeatCount="indefinite"/>
+     </rect>
+   </g><g transform="rotate(90 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"/>
+     </rect>
+   </g><g transform="rotate(120 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5833333333333334s" repeatCount="indefinite"/>
+     </rect>
+   </g><g transform="rotate(150 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5s" repeatCount="indefinite"/>
+     </rect>
+   </g><g transform="rotate(180 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.4166666666666667s" repeatCount="indefinite"/>
+     </rect>
+   </g><g transform="rotate(210 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"/>
+     </rect>
+   </g><g transform="rotate(240 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.25s" repeatCount="indefinite"/>
+     </rect>
+   </g><g transform="rotate(270 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.16666666666666666s" repeatCount="indefinite"/>
+     </rect>
+   </g><g transform="rotate(300 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.08333333333333333s" repeatCount="indefinite"/>
+     </rect>
+   </g><g transform="rotate(330 50 50)">
+     <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+       <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"/>
+     </rect>
+   </g>
+     </svg>
+ </div>
   <div v-else class="container">
     <div class="row">
       <div class="col-lg-4 col-sm-4">
@@ -100,7 +152,7 @@ export default {
     getDate(){
       setTimeout(() =>{
         this.getFilters();
-      },500)
+      },30000)
 
    },
     active() {
@@ -135,6 +187,9 @@ img.card-img {
 .inner {
   width: 100%;
   margin: auto;
+}
+.loader{
+  margin-top: 20px;
 }
 .container {
   margin: 0 auto;
